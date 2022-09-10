a11715728543816a88e8c7d071cafafe317438e3
nickson <nowembabazin8@gmail.com>
2022-09-10 12:46:23 -0500

    Edit bug

--- index.js
+++ index.js
@@ -69,6 +69,7 @@ function renderTechnologies(items) {
     const li = document.createElement('li');
     li.innerHTML = `<span>${items[i]}</span>`;
     li.classList.add('li-card-style');
+    ul.appendChild(li);
   }
   return ul.outerHTML;
 }
